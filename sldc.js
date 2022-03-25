9712aeb558281702df0a407d9f729b1abd3d0140
cdmccune <curtmccune@gmail.com>
2022-03-25 09:21:31 -0700

    Finished Variables

--- sldc.js
+++ sldc.js
@@ -5,12 +5,12 @@
     Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
 */
 
+// Planning
 // Analysis of Requirements
 // Design
 // Implementation
-// Maintain
-// Planning
 // Testing & Integration
+// Maintain
 
 
 //////////////////PROBLEM 2////////////////////
@@ -19,3 +19,8 @@
     The first one is done for you. 
 */
 const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const analysisOfRequirements = "This is the second step, in which you determine what you actually need to complete the project, what tool and technologies."
+const design = "This is the third step, in which you determine how the product is going to look and/or how all the pieces will fit together."
+const implementation = "This is the fourth step, in which you delevop through code what you planned and designed using the tools/tech you determined the project would require."
+const testingAndIntegration = "This is the fifth step, in which you test the code to see if it works, and to integrate it you make it available to the people who are going to use it on their computers."
+const maintain = "This is the last step, in which after the product has been released you continue to moniter the product and ensure that it continues to work and any bugs or problems are fixed."
